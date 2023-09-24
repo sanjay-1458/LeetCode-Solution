5ccdeaa3ce98f09f4c1d987aef10c65f9ddfb6a8
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2023-09-24 19:22:51 +0530

    Create Sol.cpp

---
+++ 81. Search in Rotated Sorted Array II/Sol.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool search(vector<int>& A, int key) {
+        int low=0,high=A.size()-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(A[mid]==key){
+                return true;
+            }
+            else if(A[mid]<=A[high]){
+                if(key>A[mid] && key<=A[high]){
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
+                }
+            }
+            else{
+                if(key>=A[low] && key<A[mid]){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
+            }
+        }
+        low=0,high=A.size()-1;
+        int mid=low+(high-low)/2;
+        if(A[low]==A[mid] && A[low]==A[high]){
+            for(int i=0;i<A.size();++i){
+                if(A[i]==key){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
