99e4aa764c9173f4355d19c695b851d97b4e5f3c
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2023-10-04 19:23:27 +0530

    Create Sol.cpp

---
+++ 1539. Kth Missing Positive Number/Sol.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findKthPositive(vector<int>& vec, int k) {
+        if(vec[0]>k) return k;
+        int n=vec.size();
+        int low=0,high=n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            int diff=vec[mid]-mid-1;
+            if(diff<k){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        int pos=high,val=0;
+        int miss=vec[pos]-pos-1;
+        int next=k-miss;
+        val=vec[pos]+next;
+        return val;
+    }
+};
