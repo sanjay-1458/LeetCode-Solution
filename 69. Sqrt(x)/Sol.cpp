c8db66d5769fffc134d58924cf9d96977eea3ba0
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2023-10-06 22:50:04 +0530

    Create Sol.cpp

---
+++ 69. Sqrt(x)/Sol.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int low=1,high=x;
+        while(low<=high){
+            long long mid=low+(high-low)/2;
+            if(mid*mid==x){
+                return mid;
+            }
+            else if(mid*mid>x){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return high;
+    }
+};
