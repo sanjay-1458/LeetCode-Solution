f80f442e70068a979b5b5321eee1e6a8c364f164
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2023-10-07 20:55:55 +0530

    Create Sol.cpp

---
+++ 1. Two Sum/Sol.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans(2,0);
+        for(int i=0;i<nums.size();++i){
+            for(int j=i+1;j<nums.size();++j){
+                if(nums[i]+nums[j]==target){
+                    ans[0]=i;
+                    ans[1]=j;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> mp;
+        for(int i=0;i<nums.size();++i){
+            mp[target-nums[i]]=i;
+        }
+        vector<int> ans(2,0);
+        for(int i=0;i<nums.size();++i){
+            if(mp.find(nums[i])!=mp.end() && i!=mp[nums[i]]){
+                ans[0]=i;
+                ans[1]=mp[nums[i]];
+                break;
+            }
+        }
+        return ans;
+    }
+};
