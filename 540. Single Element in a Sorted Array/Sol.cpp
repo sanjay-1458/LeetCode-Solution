2c28102533f6da19ad944bb16fdef4d05a49f041
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2023-10-08 21:50:33 +0530

    Update Sol.cpp

--- 540. Single Element in a Sorted Array/Sol.cpp
+++ 540. Single Element in a Sorted Array/Sol.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>& arr) {
-        if(arr.size()==1) return arr[0];
-        int low=0,high=arr.size()-1,ans=-1;
-        int first=0;
+    int singleNonDuplicate(vector<int>& nums) {
+        if(nums.size()==1 || nums[0]!=nums[1]) return nums[0];
+        if(nums[nums.size()-1]!=nums[nums.size()-2]) return nums[nums.size()-1];
+        int ans=nums[nums.size()-1];
+        int low=1,high=nums.size()-2;
         while(low<=high){
             int mid=low+(high-low)/2;
-            if (mid!=0 && mid!=arr.size()-1 && arr[mid]!=arr[mid-1] && arr[mid]!=arr[mid+1]){
-                ans=mid;
+            int start=mid;
+            if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1]){
+                ans=nums[mid];
                 break;
             }
-            if(mid==0 || mid==arr.size()-1){
-                if(mid==0 && arr[mid]!=arr[mid+1]){ans=mid;break;}
-                else if(mid==arr.size()-1 && arr[mid]!=arr[mid-1]) {ans=mid;break;}
+            else if(nums[mid]==nums[mid-1]){
+                start=mid-1;
             }
-            first=mid;
-            if(arr[mid]==arr[mid-1]){
-                first=mid-1;
+            else{
+                start=mid;
             }
-            if(first%2==0){
+            if(start%2==0){
                 low=mid+1;
             }
             else{
                 high=mid-1;
             }
         }
-        return arr[ans];
+        return ans;
     }
 };
