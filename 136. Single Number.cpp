d6692671d7fe9000f506e0007666106fb122a3b8
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2023-10-09 21:33:37 +0530

    Create 136. Single Number.cpp

---
+++ 136. Single Number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int _xor=0;
+        for(int i=0;i<nums.size();++i){
+            _xor=_xor^nums[i];
+        }
+        return _xor;
+    }
+};
