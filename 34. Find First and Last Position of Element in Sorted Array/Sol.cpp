4cb46969ccb714b2638be014edf296b72e042acf
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2023-10-10 22:51:42 +0530

    Update Sol.cpp

--- 34. Find First and Last Position of Element in Sorted Array/Sol.cpp
+++ 34. Find First and Last Position of Element in Sorted Array/Sol.cpp
@@ -1,47 +1,44 @@
 class Solution {
-    int first(vector<int>& arr,int n,int k){
+    int p1(vector<int>&nums,int k){
         int ans=-1;
-        int low=0,high=n-1;
+        int low=0,high=nums.size()-1;
         while(low<=high){
             int mid=low+(high-low)/2;
-            if(arr[mid]==k){
+            if(nums[mid]==k){
                 ans=mid;
                 high=mid-1;
             }
-            else if(arr[mid]<k){
-                low=mid+1;
+            else if(nums[mid]>k){
+                high=mid-1;
             }
             else{
-                high=mid-1;
+                low=mid+1;
             }
         }
         return ans;
     }
-
-    int last(vector<int>& arr,int n,int k){
+    int p2(vector<int>&nums,int k){
         int ans=-1;
-        int low=0,high=n-1;
+        int low=0,high=nums.size()-1;
         while(low<=high){
             int mid=low+(high-low)/2;
-            if(arr[mid]==k){
+            if(nums[mid]==k){
                 ans=mid;
                 low=mid+1;
             }
-            else if(arr[mid]<k){
-                low=mid+1;
+            else if(nums[mid]>k){
+                high=mid-1;
             }
             else{
-                high=mid-1;
+                low=mid+1;
             }
         }
         return ans;
     }
-
 public:
-    vector<int> searchRange(vector<int>& arr, int k) {
-        int n=arr.size();
-        int ans1=first(arr,n,k);
-        int ans2=last(arr,n,k);
-        return {ans1,ans2};
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int x=p1(nums,target);
+        int y=p2(nums,target);
+        return {x,y};
     }
 };
