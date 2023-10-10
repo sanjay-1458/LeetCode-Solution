e15a58d0ff3f88ebfddb633707bec9e540f3e93b
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2023-10-10 19:40:32 +0530

    Update Sol.cpp

--- 704. Binary Search/Sol.cpp
+++ 704. Binary Search/Sol.cpp
@@ -18,3 +18,26 @@ public:
         return -1;
     }
 };
+
+
+class Solution {
+    int bs(vector<int>&nums,int low,int high,int target){
+        if(low<=high){
+            int mid=(high+low)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>target){
+                return bs(nums,low,mid-1,target);
+            }
+            else{
+                return bs(nums,mid+1,high,target);
+            }
+        }
+        return -1;
+    }
+public:
+    int search(vector<int>& nums, int target) {
+        return bs(nums,0,nums.size()-1,target);
+    }
+};
