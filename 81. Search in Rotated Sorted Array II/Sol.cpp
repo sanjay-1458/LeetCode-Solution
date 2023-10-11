0e3f1e548d5cbbc6c8f5aa7bccb429c1c8d3321b
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2023-10-11 14:36:47 +0530

    Update Sol.cpp

--- 81. Search in Rotated Sorted Array II/Sol.cpp
+++ 81. Search in Rotated Sorted Array II/Sol.cpp
@@ -1,33 +1,35 @@
 class Solution {
 public:
-    bool search(vector<int>& A, int key) {
-        int low=0,high=A.size()-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(A[mid]==key){
-                return true;
-            }
-            if(A[low]==A[mid] && A[mid] == A[high]){
-                low++;high++;
-                continue;
-            }
-            if(A[mid]<=A[high]){
-                if(key>A[mid] && key<=A[high]){
-                    low=mid+1;
-                }
-                else{
-                    high=mid-1;
-                }
-            }
-            else{
-                if(key>=A[low] && key<A[mid]){
-                    high=mid-1;
-                }
-                else{
-                    low=mid+1;
-                }
-            }
-        }
-        return false;
+    bool search(vector<int>& nums, int target) {
+       int low=0,high=nums.size()-1;
+       while(low<=high){
+           int mid=low+(high-low)/2;
+           if(nums[mid]==target){
+               return true;
+           }
+           if(nums[low]==nums[high]){
+               if(nums[low]==target) return true;
+               low++;
+               high--;
+               continue;
+           }
+           if(nums[low]<=nums[mid]){
+               if(target>=nums[low] && target<nums[mid]){
+                   high=mid-1;
+               }
+               else{
+                   low=mid+1;
+               }
+           }
+           else{
+               if(target>nums[mid] && target<=nums[high]){
+                   low=mid+1;
+               }
+               else{
+                   high=mid-1;
+               }
+           }
+       }
+       return false;
     }
 };
