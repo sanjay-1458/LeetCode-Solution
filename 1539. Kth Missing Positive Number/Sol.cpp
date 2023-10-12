0439f461a82e88a529c3afcec0213fb587fa436f
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2023-10-12 21:42:09 +0530

    Update Sol.cpp

--- 1539. Kth Missing Positive Number/Sol.cpp
+++ 1539. Kth Missing Positive Number/Sol.cpp
@@ -1,23 +1,54 @@
 class Solution {
 public:
-    int findKthPositive(vector<int>& vec, int k) {
-        if(vec[0]>k) return k;
-        int n=vec.size();
-        int low=0,high=n-1;
+    int findKthPositive(vector<int>& arr, int k) {
+        if(arr[0]>k) return k;
+        int low=0,high=arr.size()-1;
         while(low<=high){
             int mid=low+(high-low)/2;
-            int diff=vec[mid]-mid-1;
-            if(diff<k){
+            int miss=arr[mid]-mid-1;
+            if(miss<k){
                 low=mid+1;
             }
             else{
                 high=mid-1;
             }
         }
-        int pos=high,val=0;
-        int miss=vec[pos]-pos-1;
-        int next=k-miss;
-        val=vec[pos]+next;
-        return val;
+        if(low==arr.size()){
+            low--;
+            int miss=arr[low]-low-1;
+            int pos=k-miss;
+            int el=arr[low]+pos;
+            return el;
+        }
+        else{
+            int miss=arr[low]-low-1;
+            int pos=miss-k+1;
+            int el=arr[low]-pos;
+            return el;
+        }
+    }
+};
+
+
+
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        if(arr[0]>k) return k;
+        int low=0,high=arr.size()-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            int miss=arr[mid]-mid-1;
+            if(miss<k){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        int miss=arr[high]-high-1;
+        int pos=k-miss;
+        int el=arr[high]+pos;
+        return el;
     }
 };
