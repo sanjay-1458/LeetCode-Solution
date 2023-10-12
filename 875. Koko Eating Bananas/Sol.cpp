20fc36340a3b5c47010943500537fc9d28f664a3
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2023-10-12 19:23:31 +0530

    Update Sol.cpp

--- 875. Koko Eating Bananas/Sol.cpp
+++ 875. Koko Eating Bananas/Sol.cpp
@@ -1,32 +1,38 @@
 class Solution {
-public:
-    int minEatingSpeed(vector<int>& piles, int h) {
-        int maxi=piles[0];
-        for(int i=0;i<piles.size();++i){
-            if(piles[i]>maxi){
-                maxi=piles[i];
+    long long int fun(vector<int>&arr,long long mid){
+        long long cnt=0;
+        for(int i=0;i<arr.size();++i){
+            cnt+=arr[i]/mid;
+            if(arr[i]%mid!=0){
+                cnt++;
             }
         }
-        long long low=1,high=maxi,ans=-1;
-        while(low<=high){
-            long long mid=low+(high-low)/2;
-            long long cnt=0;
-            for(int i=0;i<piles.size();++i){
-                if(piles[i]%mid==0){
-                    cnt+=piles[i]/mid;
-                }
-                else{
-                    cnt+=piles[i]/mid+1;
-                }
+        return cnt;
+    }
+public:
+    int minEatingSpeed(vector<int>& nums, int h) {
+        int mini=nums[0],maxi=nums[0];
+        for(int i=0;i<nums.size();++i){
+            if(nums[i]>maxi){
+                maxi=nums[i];
             }
-            if(cnt<=h){
+            if(nums[i]<mini){
+                mini=nums[i];
+            }
+        }
+        long long int low=1,high=maxi;
+        int ans=high;
+        while(low<=high){
+            long long int mid=low+(high-low)/2;
+            long long int time=fun(nums,mid);
+            if(time>h){
+                low=mid+1;
+            }
+            else{
                 ans=mid;
                 high=mid-1;
             }
-            else{
-                low=mid+1;
-            }
         }
-        return ans;
+        return low;
     }
 };
