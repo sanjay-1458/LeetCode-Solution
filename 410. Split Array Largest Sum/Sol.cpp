60a9ca40e82e4857e95f37a7e4ddb35a889f3ce5
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2023-10-13 10:29:57 +0530

    Update Sol.cpp

--- 410. Split Array Largest Sum/Sol.cpp
+++ 410. Split Array Largest Sum/Sol.cpp
@@ -40,3 +40,42 @@ public:
         return low;
     }
 };
+
+
+
+class Solution {
+    int fun(vector<int>&arr,int sub){
+        int sum=arr[0],cnt=1;
+        for(int i=1;i<arr.size();++i){
+            if(arr[i]+sum>sub){
+                sum=arr[i];
+                cnt++;
+            }
+            else{
+                sum+=arr[i];
+            }
+        }
+        return cnt;
+    }
+public:
+    int splitArray(vector<int>& nums, int k) {
+        int maxi=nums[0],sum=0;
+        for(int i=0;i<nums.size();++i){
+            if(nums[i]>maxi){
+                maxi=nums[i];
+            }
+            sum+=nums[i];
+        }
+        int low=maxi,high=sum;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(fun(nums,mid)>k){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return low;
+    }
+};
