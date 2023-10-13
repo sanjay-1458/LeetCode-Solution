1b70293899edd7f12776403f56ceb8153aea37c3
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2023-10-13 20:13:54 +0530

    Update 240. Search a 2D Matrix II.cpp

--- 240. Search a 2D Matrix II.cpp
+++ 240. Search a 2D Matrix II.cpp
@@ -20,3 +20,25 @@ public:
         return false;
     }
 };
+
+
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n=matrix.size(),m=matrix[0].size();
+        int row=0,col=m-1;
+        while(row<n && col>=0){
+            if(matrix[row][col]==target){
+                return true;
+            }
+            else if(matrix[row][col]>target){
+                col--;
+            }
+            else{
+                row++;
+            }
+        }
+        return false;
+    }
+};
