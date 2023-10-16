758b72f66627faf52031bab167d78309911cad03
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2023-10-16 20:13:44 +0530

    Update 658. Find K Closest Elements.cpp

--- 658. Find K Closest Elements.cpp
+++ 658. Find K Closest Elements.cpp
@@ -17,3 +17,29 @@ public:
         return ans;
     }
 };
+
+
+
+
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        priority_queue<pair<int,int>> pq;
+        vector<int> ans;
+        for(int i=0;i<arr.size();++i){
+            if(pq.size()==k && abs(arr[i]-x)>pq.top().first){
+                continue;
+            }
+            pq.push(make_pair(abs(arr[i]-x),arr[i]));
+            if(pq.size()>k){
+                pq.pop();
+            }
+        }
+        while(pq.size()>0){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
